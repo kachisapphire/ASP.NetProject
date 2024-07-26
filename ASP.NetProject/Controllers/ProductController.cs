50cdeeb24c880e643f34ca0c69e518f5c63ef377
kachisapphire <mychaelesther99@gmail.com>
2024-07-25 20:46:51 -0700

    completed the product functionality

--- ASP.NetProject/Controllers/ProductController.cs
+++ ASP.NetProject/Controllers/ProductController.cs
@@ -20,6 +20,7 @@ namespace ASP.NetProject.Controllers
             var products = _dbContext.Products.ToList();
             var productModelList = products.Select(x => new ProductModel
             {
+                Id = x.Id,
                 Name = x.Name,
                 Description = x.Description,
                 Price = x.Price,
@@ -47,10 +48,64 @@ namespace ASP.NetProject.Controllers
             return RedirectToAction("Index");
         }
         [HttpGet]
-		public IActionResult Edit(int Id)
+		public IActionResult Edit(int id)
         {
-            return View();
+            // look for products with this id
+            var productEntity = _dbContext.Products.Find(id);
+
+            //if this product was not found wit that id
+            // return not found
+            if (productEntity == null)
+            {
+                return View("Error");
+            }
+            // copy from product entity to product model
+            var productModel = new ProductModel
+            {
+                 Description = productEntity.Description,
+                 Price = productEntity.Price,
+                 Quantity = productEntity.Quantity,
+                 Name = productEntity.Name,
+                 Id = productEntity.Id
+            };
+            return View(productModel);
         }
+        [HttpPost]
+
+		public IActionResult Edit(ProductModel productModel)
+        {
+			// look for product with this id
+			var productEntity = _dbContext.Products.Find(productModel.Id);
+
+			//if this product was not found wit that id
+			// return not found
+			if (productEntity == null)
+			{
+				return View("Error");
+			}
+            //lets update our entity
+            productEntity.Description = productModel.Description;
+            productEntity.Price = productModel.Price;
+            productEntity.Quantity = productModel.Quantity;
+            productEntity.Name = productModel.Name;
+            // save the new updates
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+		}
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+			var productEntity = _dbContext.Products.Find(id);
+
+			if (productEntity == null)
+			{
+				return View("Error");
+			}
+            //remove product entity from product table
+            _dbContext.Products.Remove(productEntity);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+		}
 
 	}
 }
